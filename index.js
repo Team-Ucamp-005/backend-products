37769ca9fec8727e249645270ffd8aa085fe6cef
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-08-06 13:40:45 -0500

    add heroku

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 require('./db/mongodb');
+const port = process.env.PORT || 3000
 
-const PORT = 4000;
-
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${PORT}`);
 });
